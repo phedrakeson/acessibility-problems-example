a94c47e14562b041f90b28dd647327c1afcbeff4
gabe <gabrield.developer@gmail.com>
2024-07-11 20:37:40 -0300

    adds content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 
@@ -9,4 +9,15 @@ import { ButtonModule } from 'primeng/button';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  public showAlert = false;
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.showAlert = true;
+    }, 5000);
+  }
+
+  public reveal(): void {
+    document.getElementById('body')?.classList.toggle('visible');
+  }
+}
